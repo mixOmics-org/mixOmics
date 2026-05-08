300a063b7f5f28f419adc7435a22f0a37f4932a6
Derek Lei <leiderek12@gmail.com>
2026-05-08 11:50:43 +1000

    test: rewrite mint.block tests

--- tests/testthat/test-mint.block.pls.R
+++ tests/testthat/test-mint.block.pls.R
@@ -1,17 +1,62 @@
 context("mint.block.pls")
 
-# Test
-test_that("mint.block.pls works", {
+.mint_block_pls_data <- function(nvar = NULL) {
   data(breast.TCGA)
-  study = c(rep("study1", 150), rep("study2", 70))
-  mrna = rbind(breast.TCGA$data.train$mrna, breast.TCGA$data.test$mrna)
-  mirna = rbind(breast.TCGA$data.train$mirna, breast.TCGA$data.test$mirna)
-  Y = mrna[, 1]
-  mrna = mrna[, -1]
-  data = list(mrna = mrna, mirna = mirna)
-  res = mint.block.plsda(data, Y, study=study, ncomp=2)
-  expect_equal(res$loadings$mrna[5], -0.08031856, tolerance = 1e-5)
+  X <- lapply(c("mrna", "mirna"), function(block) {
+    x <- rbind(breast.TCGA$data.train[[block]], breast.TCGA$data.test[[block]])
+    if (is.null(nvar)) x else x[, seq_len(nvar)]
+  })
+  names(X) <- c("mrna", "mirna")
+  Y <- as.matrix(X$mrna[, 1])
+  colnames(Y) <- "Y"
+  X$mrna <- X$mrna[, -1]
+  list(X = X, Y = Y, study = rep(c("study1", "study2"), c(150, 70)))
+}
+
+test_that("(mint.block.pls:basic): breast.TCGA", {
+  data <- .mint_block_pls_data()
+  res <- mint.block.pls(data$X, data$Y, study = data$study, ncomp = 2)
+
+  expect_true("mint.block.pls" %in% class(res))
+  expect_equal(res$loadings$mrna[5], -0.08196021, tolerance = 1e-5)
   expect_equal(res$mode, "regression")
 })
-  
-               
+
+test_that("(mint.block.pls:data): Y supplied through 'indY'", {
+  data <- .mint_block_pls_data(nvar = 20)
+  res <- mint.block.pls(c(data$X, list(Y = data$Y)), indY = 3,
+                        study = data$study, ncomp = 1)
+
+  expect_true("mint.block.pls" %in% class(res))
+  expect_equal(res$ncomp, c(mrna = 1, mirna = 1, Y = 1))
+})
+
+test_that("(mint.block.pls:parameter): block-only 'design'", {
+  data <- .mint_block_pls_data(nvar = 20)
+  design <- matrix(c(0, 0.5, 0.5, 0), nrow = 2)
+
+  expect_message(
+    res <- mint.block.pls(data$X, data$Y, study = data$study,
+                          design = design, ncomp = 1),
+    "Design matrix has changed to include Y"
+  )
+  expect_true("mint.block.pls" %in% class(res))
+  expect_equal(res$ncomp["Y"], c(Y = 1))
+})
+
+test_that("(mint.block.pls:error): invalid model inputs", {
+  data <- .mint_block_pls_data(nvar = 20)
+
+  expect_error(mint.block.pls(data$X, study = data$study),
+               "Either 'Y' or 'indY' is needed", fixed = TRUE)
+  expect_error(mint.block.pls(data$X, data$Y[, 1], study = data$study),
+               "'Y' must be a numeric matrix.", fixed = TRUE)
+})
+
+test_that("(mint.block.pls:edge.case): missing 'study'", {
+  data <- .mint_block_pls_data(nvar = 20)
+  res <- mint.block.pls(data$X, data$Y, ncomp = 1)
+
+  expect_true("mint.block.pls" %in% class(res))
+  expect_equal(table(res$study)[["1"]], nrow(data$X$mrna))
+})
--- tests/testthat/test-mint.block.spls.R
+++ tests/testthat/test-mint.block.spls.R
@@ -1,18 +1,67 @@
 context("mint.block.spls")
 
-# Test
-test_that("mint.block.spls works", {
+.mint_block_spls_data <- function(nvar = NULL) {
   data(breast.TCGA)
-  study = c(rep("study1", 150), rep("study2", 70))
-  mrna = rbind(breast.TCGA$data.train$mrna, breast.TCGA$data.test$mrna)
-  mirna = rbind(breast.TCGA$data.train$mirna, breast.TCGA$data.test$mirna)
-  Y = mrna[, 1]
-  mrna = mrna[, -1]
-  data = list(mrna = mrna, mirna = mirna)
-  res = mint.block.splsda(data, Y, study=study, ncomp=2,
-                          keepX = list(mrna=c(10,10), mirna=c(20,20)))
+  X <- lapply(c("mrna", "mirna"), function(block) {
+    x <- rbind(breast.TCGA$data.train[[block]], breast.TCGA$data.test[[block]])
+    if (is.null(nvar)) x else x[, seq_len(nvar)]
+  })
+  names(X) <- c("mrna", "mirna")
+  Y <- as.matrix(X$mrna[, 1])
+  colnames(Y) <- "Y"
+  X$mrna <- X$mrna[, -1]
+  list(X = X, Y = Y, study = rep(c("study1", "study2"), c(150, 70)))
+}
+
+test_that("(mint.block.spls:basic): breast.TCGA", {
+  data <- .mint_block_spls_data()
+  res <- mint.block.spls(data$X, data$Y, study = data$study, ncomp = 2,
+                         keepX = list(mrna = c(10, 10), mirna = c(20, 20)),
+                         keepY = c(1, 1))
+
+  expect_true("mint.block.spls" %in% class(res))
   expect_equal(res$loadings$mrna[5], 0, tolerance = 1e-5)
   expect_equal(res$mode, "regression")
 })
-  
-               
+
+test_that("(mint.block.spls:data): Y supplied through 'indY'", {
+  data <- .mint_block_spls_data(nvar = 20)
+  res <- mint.block.spls(c(data$X, list(Y = data$Y)), indY = 3,
+                         study = data$study, ncomp = 1,
+                         keepX = list(mrna = 2, mirna = 3))
+
+  expect_true("mint.block.spls" %in% class(res))
+  expect_equal(res$keepX$comp1$Y, 1)
+  expect_null(res$keepY)
+})
+
+test_that("(mint.block.spls:parameter): partially specified 'keepX' and 'keepY'", {
+  data <- .mint_block_spls_data(nvar = 20)
+  res <- mint.block.spls(data$X, data$Y, study = data$study, ncomp = 2,
+                         keepX = list(mrna = 2), keepY = 1)
+
+  expect_equal(res$keepX$comp1$mrna, 2)
+  expect_equal(res$keepX$comp2$mrna, ncol(data$X$mrna))
+  expect_equal(res$keepX$comp1$Y, ncol(data$Y))
+})
+
+test_that("(mint.block.spls:error): invalid model inputs", {
+  data <- .mint_block_spls_data(nvar = 20)
+
+  expect_error(mint.block.spls(data$X, study = data$study),
+               "Either 'Y' or 'indY' is needed", fixed = TRUE)
+  expect_error(mint.block.spls(data$X, data$Y[, 1], study = data$study),
+               "'Y' must be a numeric matrix.", fixed = TRUE)
+  expect_error(mint.block.spls(data$X, data$Y, study = data$study,
+                               keepX = list(bad = c(1, 1)), ncomp = 2),
+               "Each entry of 'keepX' must have a unique name", fixed = TRUE)
+})
+
+test_that("(mint.block.spls:edge.case): single component", {
+  data <- .mint_block_spls_data(nvar = 20)
+  res <- mint.block.spls(data$X, data$Y, study = data$study, ncomp = 1,
+                         keepX = list(mrna = 2, mirna = 3), keepY = 1)
+
+  expect_true("mint.block.spls" %in% class(res))
+  expect_equal(ncol(res$variates$mrna), 1L)
+})
