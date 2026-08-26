dc45785caf70d1e3990c37790e2d3d5eb71ce977
Derek Lei <derek.lei@mixomics.org>
2026-08-26 11:34:05 +1000

    test: add mint.block.splsda structure and predict tests

--- tests/testthat/test-mint.block.splsda.R
+++ tests/testthat/test-mint.block.splsda.R
@@ -45,6 +45,73 @@ test_that("mint.block.plsda and mint.block.splsda works", {
 ### ============================== PARAMETER ============================== ###
 ###############################################################################
 
+test_that("mint.block.plsda returns the DA block-object structure and predicts", {
+
+  data(breast.TCGA)
+  mrna <- rbind(breast.TCGA$data.train$mrna, breast.TCGA$data.test$mrna)
+  mirna <- rbind(breast.TCGA$data.train$mirna, breast.TCGA$data.test$mirna)
+  X <- list(mrna = mrna, mirna = mirna)
+  Y <- c(breast.TCGA$data.train$subtype, breast.TCGA$data.test$subtype)
+
+  study <- c(rep("study1",150), rep("study2",70))
+
+  res <- mint.block.plsda(X, Y, study = study, ncomp = 2)
+
+  expect_true(all(c("mint.block.plsda", "mint.block.pls", "block.plsda", "block.pls")
+                  %in% class(res)))
+
+  # X excludes the outcome block; Y is the original factor
+  expect_equal(names(res$X), c("mrna", "mirna"))
+  expect_true(is.factor(res$Y))
+  expect_equal(dim(res$ind.mat), c(220, 3))
+  expect_equal(res$indY, 3)
+  expect_equal(rownames(res$weights), c("mrna", "mirna"))
+
+  pred <- predict(res, newdata = X, study.test = study)
+  expect_true(all(c("predict", "class", "MajorityVote", "WeightedVote") %in% names(pred)))
+})
+
+test_that("mint.block.splsda returns correct keepX/keepY and predicts", {
+
+  data(breast.TCGA)
+  mrna <- rbind(breast.TCGA$data.train$mrna, breast.TCGA$data.test$mrna)
+  mirna <- rbind(breast.TCGA$data.train$mirna, breast.TCGA$data.test$mirna)
+  X <- list(mrna = mrna, mirna = mirna)
+  Y <- c(breast.TCGA$data.train$subtype, breast.TCGA$data.test$subtype)
+
+  study <- c(rep("study1",150), rep("study2",70))
+
+  res <- mint.block.splsda(X, Y, study = study, ncomp = 2,
+                           keepX = list(mrna = c(10, 10), mirna = c(20, 20)))
+
+  expect_true(all(c("mint.block.splsda", "mint.block.spls", "block.splsda", "block.spls")
+                  %in% class(res)))
+
+  # keepX returned as supplied, keepY defaults to the number of outcome levels
+  expect_equal(res$keepX, list(mrna = c(10, 10), mirna = c(20, 20)))
+  expect_equal(as.numeric(res$keepY), c(3, 3))
+  expect_equal(unname(colSums(res$loadings$mrna != 0)), c(10, 10))
+  expect_equal(unname(colSums(res$loadings$mirna != 0)), c(20, 20))
+
+  # X excludes the outcome block; Y is the original factor
+  expect_equal(names(res$X), c("mrna", "mirna"))
+  expect_true(is.factor(res$Y))
+  expect_equal(dim(res$ind.mat), c(220, 3))
+  expect_equal(res$indY, 3)
+  expect_equal(rownames(res$weights), c("mrna", "mirna"))
+
+  # selectVar excludes the outcome block
+  sv <- selectVar(res, comp = 1)
+  expect_equal(names(sv), c("mrna", "mirna", "comp"))
+
+  # predict works, including the weighted outputs
+  pred <- predict(res, newdata = X, study.test = study)
+  expect_true(all(c("predict", "class", "MajorityVote", "WeightedVote",
+                    "AveragedPredict", "WeightedPredict") %in% names(pred)))
+  acc <- mean(pred$class$max.dist[["mrna"]][, 2] == as.character(Y))
+  expect_gt(acc, 0.7)
+})
+
 
 
 
